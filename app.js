d999cc5cee77d3dd9e879903d85e58d765530180
John Park <johpar24@bergen.org>
2023-02-07 15:59:19 -0500

    initial commit

--- app.js
+++ app.js
@@ -3,6 +3,11 @@ const express = require( "express" );
 const logger = require("morgan");
 const app = express();
 const port = 8080;
+const db = require('./db/db_connection');
+
+// Configure Express to use EJS
+app.set( "views",  __dirname + "/views");
+app.set( "view engine", "ejs" );
 
 // define middleware that logs all incoming requests
 app.use(logger("dev"));
@@ -12,18 +17,40 @@ app.use(express.static(__dirname + '/public'));
 
 // define a route for the default home page
 app.get( "/", ( req, res ) => {
-    res.sendFile( __dirname + "/views/index.html" );
-} );
+    res.render('index');
+});
 
 // define a route for the stuff inventory page
+const read_stuff_all_sql = `
+    SELECT 
+        CountryID, country_name, country_population, country_gdp
+    FROM
+        Countries
+`
 app.get( "/stuff", ( req, res ) => {
-    res.sendFile( __dirname + "/views/stuff.html" );
-} );
+    db.execute(read_stuff_all_sql, (error, results) => {
+        if (error)
+            res.status(500).send(error); //Internal Server Error
+        else
+            res.send(results);
+    });
+});
 
 // define a route for the item detail page
-app.get( "/stuff/item", ( req, res ) => {
-    res.sendFile( __dirname + "/views/item.html" );
-} );
+app.get( "/stuff/item/:id", ( req, res ) => {
+    db.execute(read_item_sql, [req.params.id], (error, results) => {
+        if (error)
+            res.status(500).send(error); //Internal Server Error
+        else if (results.length == 0)
+            res.status(404).send(`No item found with id = "${req.params.id}"` ); // NOT FOUND
+        else {
+            let data = results[0]; // results is still an array
+            // data's object structure: 
+            //  { item: ___ , quantity:___ , description: ____ }
+            res.render('item', data);
+        }
+    });
+});
 
 // start the server
 app.listen( port, () => {
