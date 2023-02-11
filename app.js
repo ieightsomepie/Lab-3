77d0a5fe4b5f8b1ddb44dc97d9c6f5503e65e5f0
John Park <johpar24@bergen.org>
2023-02-10 23:02:31 -0500

    initial commit

--- app.js
+++ app.js
@@ -12,6 +12,9 @@ app.set( "view engine", "ejs" );
 // define middleware that logs all incoming requests
 app.use(logger("dev"));
 
+// Configure Express to parse URL-encoded POST request bodies (traditional forms)
+app.use( express.urlencoded({ extended: false }) );
+
 // define middleware that serves static resources in the public directory
 app.use(express.static(__dirname + '/public'));
 
@@ -29,38 +32,93 @@ const read_stuff_all_sql = `
 `
 app.get( "/stuff", ( req, res ) => {
     db.execute(read_stuff_all_sql, (error, results) => {
+        console.log(results)
         if (error)
             res.status(500).send(error); //Internal Server Error
         else {
-            res.render('stuff', { inventory : results });
+            res.render('stuff', { Countries : results });
         }
     });
 } );
 
 // define a route for the item detail page
-const read_item_sql = `
+const read_stuff_item_sql = `
     SELECT 
-        country_name, country_population, country_gdp 
+        CountryID, country_name, country_population, country_gdp
     FROM
         Countries
-    WHERE
-        CountryID = 1
+    WHERE 
+        CountryID = ?    
 `
 app.get( "/stuff/item/:id", ( req, res ) => {
-    db.execute(read_item_sql, [req.params.id], (error, results) => {
-        if (error)
-            res.status(500).send(error); //Internal Server Error
-        else if (results.length == 0)
-            res.status(404).send(`No item found with id = "${req.params.id}"` ); // NOT FOUND
+    db.execute(read_stuff_item_sql, [req.params.id], (error, results) => {
+        if (error) {
+            res.status(500).send(error); // Internal Server Error
+        } else if (results.length == 0) {
+            res.status(404).send(`No item found with id = '${req.params.id}'`);
+        }
         else {
-            let data = results[0]; // results is still an array
-            // data's object structure: 
-            //  { item: ___ , quantity:___ , description: ____ }
+            let data = results[0];
+            //data = {item: ____, quantity: ____, description: ____ }
             res.render('item', data);
         }
     });
-});
 
+} );
+// define a route for item DELETE
+const delete_item_sql = `
+    DELETE 
+    FROM
+        Countries
+    WHERE
+        CountryID = ?
+`
+app.get("/stuff/item/:id/delete", ( req, res ) => {
+    db.execute(delete_item_sql, [req.params.id], (error, results) => {
+        if (error)
+            res.status(500).send(error); //Internal Server Error
+        else {
+            res.redirect("/stuff");
+        }
+    });
+})
+// define a route for item Create
+const create_item_sql = `
+    INSERT INTO Countries
+        (country_name, country_population, country_gdp)
+    VALUES
+        (?, ?, ?)
+`
+app.post("/stuff", ( req, res ) => {
+    db.execute(create_item_sql, [req.body.country_name, req.body.country_population, req.body.country_gdp], (error, results) => {
+        if (error)
+            res.status(500).send(error); //Internal Server Error
+        else {
+            //results.insertId has the primary key (id) of the newly inserted element.
+            res.redirect(`/stuff/item/${results.insertId}`);
+        }
+    });
+})
+// define a route for item UPDATE
+const update_item_sql = `
+    UPDATE
+        Countries
+    SET
+        country_name = ?,
+        country_population = ?,
+        country_gdp = ?
+    WHERE
+        CountryID = ?
+`
+app.post("/stuff/item/:CountryID", ( req, res ) => {
+    db.execute(update_item_sql, [req.body.country_name, req.body.country_population, req.body.country_gdp, req.params.id], (error, results) => {
+        if (error)
+            res.status(500).send(error); //Internal Server Error
+        else {
+            res.redirect(`/stuff/item/${req.params.id}`);
+        }
+    });
+})
 // start the server
 app.listen( port, () => {
     console.log(`App server listening on ${ port }. (Go to http://localhost:${ port })` );
